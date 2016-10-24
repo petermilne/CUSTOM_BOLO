3af33d1da2df8174dbf8fe42f11283c893603ceb
Peter Milne <peter.milne@d-tacq.com>
2016-10-24 14:31:10 +0100

    major cleanout of LIA crud. Question is, why not long ago?0 	deleted:    CARE/LIA/acq400_streamd.0.conf.lia 	deleted:    CARE/LIA/external-clock-intref-setup 	deleted:    CARE/LIA/initialise_5REF 	deleted:    CARE/LIA/intref.m 	deleted:    CARE/LIA/lia-intref-1024 	deleted:    CARE/LIA/lia-regdef 	deleted:    CARE/LIA/set.default_ref_mapping 	deleted:    CARE/LIA/sysconfig/acq424-32-transient.init 	deleted:    CARE/LIA/sysconfig/acq425-16-transient.init 	deleted:    CARE/LIA/sysconfig/epics.sh 	deleted:    CARE/hanning-float.bin 	modified:   README 	new file:   init/custom_bolo.init 	deleted:    init/custom_lia.init 	modified:   make.package

--- CARE/LIA/intref.m
+++
@@ -1,6 +0,0 @@
-t=1:1024;
-y=32767*sin(2*pi*t/1024);
-fp=fopen("lia-intref-1024","w");
-fwrite(fp,y,"int32");
-fclose(fp)
-
--- CARE/LIA/sysconfig/epics.sh
+++
@@ -1,5 +0,0 @@
-export EPICS_CA_MAX_ARRAY_BYTES=500000
-export IOC_PREINIT=/usr/local/epics/scripts/make-lia-aliases
-# allow more than 80 "channels" per module eg for LIA
-export ACQ400IOC_IDX_GT32=1
-
